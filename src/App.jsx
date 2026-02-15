c70b19c01cf5ebe3b9f6db4f66f354e9caad0db0
dini <emailsrm01@gmail.com>
2026-02-15 08:24:05 +0530

    products ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+import Checkout from './pages/Checkout'
+import Navbar from './mycomp/Navbar'
 
 
 const App = () => {
   return (
     <div>
-      <Button>Click Me</Button>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/checkout' element={<Checkout />} />
+      </Routes>
 
     </div>
   )
---
+++ src/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+    {
+        id: 1,
+        name: "Product 1",
+        price: 100,
+        description: "Description of Product 1",
+        image: "https://picsum.photos/id/1/200",
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        price: 200,
+        description: "Description of Product 2",
+        image: "https://picsum.photos/id/2/200",
+    },
+    {
+        id: 3,
+        name: "Product 3",
+        price: 300,
+        description: "Description of Product 3",
+        image: "https://picsum.photos/id/3/200",
+    },
+    {
+        id: 4,
+        name: "Product 4",
+        price: 400,
+        description: "Description of Product 4",
+        image: "https://picsum.photos/id/4/200",
+    },
+    {
+        id: 5,
+        name: "Product 5",
+        price: 500,
+        description: "Description of Product 5",
+        image: "https://picsum.photos/id/5/200",
+    },
+    {
+        id: 6,
+        name: "Product 6",
+        price: 600,
+        description: "Description of Product 6",
+        image: "https://picsum.photos/id/6/200",
+    },
+    {
+        id: 7,
+        name: "Product 7",
+        price: 700,
+        description: "Description of Product 7",
+        image: "https://picsum.photos/id/7/200",
+    },
+    {
+        id: 8,
+        name: "Product 8",
+        price: 800,
+        description: "Description of Product 8",
+        image: "https://picsum.photos/id/8/200",
+    },
+    {
+        id: 9,
+        name: "Product 9",
+        price: 900,
+        description: "Description of Product 9",
+        image: "https://picsum.photos/id/9/200",
+    },
+    {
+        id: 10,
+        name: "Product 10",
+        price: 1000,
+        description: "Description of Product 10",
+        image: "https://picsum.photos/id/10/200",
+    },
+]
+
+export const getProducts = () => {
+    return products
+}
